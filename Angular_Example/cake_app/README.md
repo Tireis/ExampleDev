ecd407c2fb8699b1d7177e315cc223787c959b0d
Tim Tabert <tim.tabert@weidmueller.com>
2025-02-17 15:39:33 +0100

    first views with components created

--- Angular_Example/cake_app/README.md
+++ Angular_Example/cake_app/README.md
@@ -25,3 +25,12 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+```ts
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'popular', component: PopularComponent },
+  { path: 'add', component: AddRecipeComponent },
+];
+```
--- Angular_Example/cake_app/src/app/app.routes.ts
+++ Angular_Example/cake_app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { PopularComponent } from './popular/popular.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'popular', component: PopularComponent },
+  { path: 'add', component: AddRecipeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
